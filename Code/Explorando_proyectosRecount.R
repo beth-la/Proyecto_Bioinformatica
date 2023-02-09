3ba49c3dbce9996ac1419e6bb68cf34556fcb965
Brenda Elizabeth Lopez Angeles <blopez@lcg.unam.mx>
2023-02-08 19:53:20 -0600

    Explorando los proyectos disponibles en recount3

---
+++ Code/Explorando_proyectosRecount.R
@@ -0,0 +1,19 @@
+# Buscando Proyecto
+## Cambiar el URL de recount3 a Amazon (AWS)
+
+getOption(
+  "recount3_url",
+  "http://duffel.rail.bio/recount3"
+)
+
+options(recount3_url = "https://recount-opendata.s3.amazonaws.com/recount3/release")
+
+## Confirmando que se cambió el URL
+getOption(
+  "recount3_url",
+  "http://duffel.rail.bio/recount3"
+)
+
+## Vemos los proyectos disponibles de raton
+mouse_projects <- available_projects(organism = "mouse")
+#PROYECTO 147
