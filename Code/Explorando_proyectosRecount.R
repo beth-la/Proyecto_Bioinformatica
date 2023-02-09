2721eb5f1104a4bdf43227a6f720470099ebb471
Brenda Elizabeth Lopez Angeles <blopez@lcg.unam.mx>
2023-02-08 21:27:56 -0600

    Haciendo pruebas con recount y eligiendo proyecto

--- Code/Explorando_proyectosRecount.R
+++ Code/Explorando_proyectosRecount.R
@@ -16,4 +16,13 @@ getOption(
 
 ## Vemos los proyectos disponibles de raton
 mouse_projects <- available_projects(organism = "mouse")
-#PROYECTO 147
+
+#Probando proyecto 147 SRP118914
+project_info <- subset(
+  mouse_projects,
+  project == "SRP118914"
+)
+rse_gene_SRP118914 <- create_rse(project_info)
+assay(rse_gene_SRP118914, "counts") <- compute_read_counts(rse_gene_SRP118914)
+
+# Se decidio usar el proyecto anterior
